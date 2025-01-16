5a0c4423b8ad343839bfa758700e04c2f0d5bc37
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-01-16 13:48:31 -0500

    webpack settings specific for dev

---
+++ webpack.dev.js
@@ -0,0 +1,17 @@
+
+
+
+let {merge} = require("webpack-merge");
+
+
+let common = require("./webpack.common");
+
+
+
+module.exports = merge(common, {
+    mode : "development",
+    devtool : "inline-source-map",
+    devServer : {
+        static : "./dist"
+    }
+})
