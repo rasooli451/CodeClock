91358ff24b24093f9fd76395ee9c9e86503d1087
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-01-16 13:48:11 -0500

    webpack settings common for both dev and prod

---
+++ webpack.common.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+const path = require("path");
+
+let HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry : "./source/index.js",
+    output : {
+        filename : "bundle.js",
+        path : path.resolve(__dirname, "dist")
+    },
+
+    plugins : [
+        new HtmlWebpackPlugin({
+            title : "CodeClock",
+            template : "./source/index.html",
+            inject : "body"
+        })
+    ],
+
+    module : {
+        rules : [
+            {
+                test : /\.css$/i,
+                use : ["style-loader", "css-loader"]
+            },
+            {
+                test : /\.(png|svg|jpg|jpeg|gif)$/i,
+                type : "asset/resource",
+            },
+            {
+                test : /\.html$/i,
+                loader : "html-loader"
+            },
+            {
+                test : /\.(woff|woff2|eot|ttf|otf)$/i,
+                type : "asset/resource"
+            }
+        ]
+    }
+}
